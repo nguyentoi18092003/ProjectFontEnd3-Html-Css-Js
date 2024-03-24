0af4b86fdd77d2e6e9a2e25f5758b12312106ec3
Nguyen Thi Toi <nguyentoi18092003hvcnbcvt@gmail.com>
2024-03-24 23:16:26 +0700

    Tinh nang hien thi san pham

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+//1
+import { fetchApi } from "./fetchApi.js";
+
+const product=document.querySelector("#product");
+//dc lay tu 1
+fetchApi("http://localhost:3000/products")
+.then(data=>{
+    
+    const arrayHTML=data.map(item=>{
+        return `
+        <div class="product_item">
+            <div class="product_image">
+                <img src="https://gcs.tripi.vn/public-tripi/tripi-feed/img/474076SPG/cach-doi-dinh-dang-anh-jpg-truc-tiep-tren-iphone_022454763.jpg" alt="${item.title}"/>
+                <div class="product_percent">
+                    ${item.discountPercentage}%
+                </div>
+            </div>
+            <div class="product_content">
+                <h3 class="product_title">${item.title}</h3>
+                <div class="product_meta">
+                    <div class="product_price">
+                        ${item.price}
+                    </div>
+                    <div class="product_stock">
+                        Còn lại: ${item.stock}
+                    </div>
+                </div>
+            </div>
+        </div>
+        `;
+    })
+    const stringHTML=arrayHTML.join("");
+    console.log(stringHTML);
+    product.innerHTML=stringHTML;
+})
